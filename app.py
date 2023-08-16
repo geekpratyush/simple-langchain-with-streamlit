d4dc78131ed3268d6aca8161fd46099c03e8e2ca
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-08-16 13:17:18 +0530

    Clear Chat History.

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)    
+
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
