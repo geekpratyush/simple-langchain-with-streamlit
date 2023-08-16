ee89897199ff80e2c0534e743d1b660d0bf77e28
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-08-16 10:02:12 +0530

    some simple changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import openai
 import streamlit as st
 
+
+st.header('A BasicGPT __by_ :blue[Pratyush Mishra] emojis :sunglasses:')
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
     if 'OPENAI_API_KEY' in st.secrets:
