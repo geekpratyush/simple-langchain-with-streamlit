20f9bcf8637aefd585ecdf4425e4cccb978566c1
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-08-16 13:16:13 +0530

    Moved the creator details to somewhere else.

--- app.py
+++ app.py
@@ -18,6 +18,16 @@ with st.sidebar:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)    
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -40,5 +50,9 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history) 
 
 
