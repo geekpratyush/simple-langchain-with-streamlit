7fb6f621943cd3606f73e7a86347aead68258f63
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-08-16 14:59:22 +0530

    Emojis.

--- app.py
+++ app.py
@@ -27,10 +27,10 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    with open('creator.txt', 'r') as file:
-        creator_content = file.read()
+with open('creator.txt', 'r') as file:
+    creator_content = file.read()
     st.session_state.messages.append({"role": "system", "content": creator_content})
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
