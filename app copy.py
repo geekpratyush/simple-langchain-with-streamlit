87f84e560bbef68f7f0b0f0b3fbcc046a051fa44
geekpratyush <pratyush.ranjan.mishra@gmail.com>
2023-08-16 15:43:42 +0530

    trimming contents

---
+++ app copy.py
@@ -0,0 +1,52 @@
+import openai
+import streamlit as st
+from transformers import pipeline
+
+
+# def clear_chat_history():
+#     st.session_state.messages = []
+
+st.header('A BasicGPT _by_ :blue[Pratyush Ranjan Mishra] :sunglasses:')
+with st.sidebar:
+    st.title('🤖💬 OpenAI Chatbot :flag-in:')
+    if 'OPENAI_API_KEY' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        openai.api_key = st.secrets['OPENAI_API_KEY']
+    else:
+        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    #st.sidebar.button('Clear Chat History', on_click=clear_chat_history) 
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+with open('creator.txt', 'r') as file:
+    creator_content = file.read()
+    st.session_state.messages.append({"role": "system", "content": creator_content})
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]}
+                      for m in st.session_state.messages], stream=True):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+
+
+
