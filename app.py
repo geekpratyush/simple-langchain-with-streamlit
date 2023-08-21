9355441f698c020bf383da6a9f5f03fd5f8216c0
Pratyush Ranjan Mishra <pratyush.ranjan.mishra@gmail.com>
2023-08-21 22:53:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+with st.sidebar:
+    openai_api_key = text_input('OpenAI API Key', type='password')
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
