ca969fdf0b727302bbb7ef3729b77f8fb8c02b02
Pratyush Ranjan Mishra <pratyush.ranjan.mishra@gmail.com>
2023-08-21 21:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.write('Pratyush\'s Generative AI *World!* :sunglasses:')
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
-st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+st.markdown(":red[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
