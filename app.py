9ee68f9e5ccc8fa72735b6a75495c8619c6373d3
Pratyush Ranjan Mishra <pratyush.ranjan.mishra@gmail.com>
2023-08-21 22:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ st.write("I'm ", age, 'years old')
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.text_input("What is up?"):
   st.write(prompt)
