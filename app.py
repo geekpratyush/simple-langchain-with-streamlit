b59b7a326e2da8f829fdfde04a866f8841943edb
Pratyush Ranjan Mishra <pratyush.ranjan.mishra@gmail.com>
2023-08-21 22:30:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import streamlit as st
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot :flag-in:')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+    api_key = st.text_input('Enter OpenAI API token:', type='password')
+    if not (api_key.startswith('sk-') and len(api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
