cebbe76e553149ba78e57b8ee7bc2f8fb83c8af1
Pratyush Ranjan Mishra <pratyush.ranjan.mishra@gmail.com>
2023-08-21 22:33:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot :flag-in:')
     api_key = st.text_input('Enter OpenAI API token:', type='password')
+    llm = OpenAI(openai_api_key=api_key)
     if not (api_key.startswith('sk-') and len(api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
     else:
